7c784a24386465f5c85b459ea363b2711a7a990a
Anwar Hussen <anwar.hussen4@gmail.com>
2018-07-22 15:41:58 +0600

    Added caller id functions

--- src/ad101tester/AD101Tester.java
+++ src/ad101tester/AD101Tester.java
@@ -5,10 +5,12 @@
  */
 package ad101tester;
 
+import ad101tester.AD101Device.AD101DEVICEPARAMETER;
 import static java.lang.System.out;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -26,30 +28,30 @@ public class AD101Tester extends Application {
     private AD101Device cid = AD101Device.INSTANCE;
 
     //params
-    int MCU_BACKID = 0x07;	// Return Device ID
-    int MCU_BACKSTATE = 0x08;	// Return Device State
-    int MCU_BACKCID = 0x09;		// Return Device CallerID
-    int MCU_BACKDIGIT = 0x0A;	// Return Device Dial Digit
-    int MCU_BACKDEVICE = 0x0B;	// Return Device Back Device ID
-    int MCU_BACKPARAM = 0x0C;	// Return Device Paramter
-    int MCU_BACKCPUID = 0x0D;	// Return Device CPU ID
-    int MCU_BACKCOLLATERAL = 0x0E;		// Return Collateral phone dialed
-    int MCU_BACKDISABLE = 0xFF;		// Return Device Init
-    int MCU_BACKENABLE = 0xEE;
-    int MCU_BACKMISSED = 0xAA;		// Missed call 
-    int MCU_BACKTALK = 0xBB;		// Start Talk
+    final int MCU_BACKID = 0x07;	// Return Device ID
+    final int MCU_BACKSTATE = 0x08;	// Return Device State
+    final int MCU_BACKCID = 0x09;		// Return Device CallerID
+    final int MCU_BACKDIGIT = 0x0A;	// Return Device Dial Digit
+    final int MCU_BACKDEVICE = 0x0B;	// Return Device Back Device ID
+    final int MCU_BACKPARAM = 0x0C;	// Return Device Paramter
+    final int MCU_BACKCPUID = 0x0D;	// Return Device CPU ID
+    final int MCU_BACKCOLLATERAL = 0x0E;		// Return Collateral phone dialed
+    final int MCU_BACKDISABLE = 0xFF;		// Return Device Init
+    final int MCU_BACKENABLE = 0xEE;
+    final int MCU_BACKMISSED = 0xAA;		// Missed call 
+    final int MCU_BACKTALK = 0xBB;		// Start Talk
 
-    int HKONSTATEPRA = 0x01; // hook on pr+  HOOKON_PRA
-    int HKONSTATEPRB = 0x02;  // hook on pr-  HOOKON_PRR
-    int HKONSTATENOPR = 0x03;  // have pr  HAVE_PR
-    int HKOFFSTATEPRA = 0x04;   // hook off pr+  HOOKOFF_PRA
-    int HKOFFSTATEPRB = 0x05;  // hook off pr-  HOOKOFF_PRR
-    int NO_LINE = 0x06; // no line  NULL_LINE
-    int RINGONSTATE = 0x07;  // ring on  RING_ON
-    int RINGOFFSTATE = 0x08;  // ring off RING_OFF
-    int NOHKPRA = 0x09; // NOHOOKPRA= 0x09, // no hook pr+
-    int NOHKPRB = 0x0a; // NOHOOKPRR= 0x0a, // no hook pr-
-    int NOHKNOPR = 0x0b; // NOHOOKNPR= 0x0b, // no hook no pr
+    final int HKONSTATEPRA = 0x01; // hook on pr+  HOOKON_PRA
+    final int HKONSTATEPRB = 0x02;  // hook on pr-  HOOKON_PRR
+    final int HKONSTATENOPR = 0x03;  // have pr  HAVE_PR
+    final int HKOFFSTATEPRA = 0x04;   // hook off pr+  HOOKOFF_PRA
+    final int HKOFFSTATEPRB = 0x05;  // hook off pr-  HOOKOFF_PRR
+    final int NO_LINE = 0x06; // no line  NULL_LINE
+    final int RINGONSTATE = 0x07;  // ring on  RING_ON
+    final int RINGOFFSTATE = 0x08;  // ring off RING_OFF
+    final int NOHKPRA = 0x09; // NOHOOKPRA= 0x09, // no hook pr+
+    final int NOHKPRB = 0x0a; // NOHOOKPRR= 0x0a, // no hook pr-
+    final int NOHKNOPR = 0x0b; // NOHOOKNPR= 0x0b, // no hook no pr
 
     int WM_USBLINEMSG = 1024 + 180;
 
@@ -87,7 +89,11 @@ public class AD101Tester extends Application {
                 //setting call back function
                 Thread t = new Thread(() -> {
                     cid.AD101_SetEventCallbackFun((int iLine, int iEvent, int iParam) -> {
-                        System.out.println("Incoming from line: " + iLine + " event: " + Integer.toHexString(iEvent) + " param: " + Integer.toHexString(iParam));
+                        System.out.println("Line: " + iLine + " event: " + Integer.toHexString(iEvent)
+                                + " param: " + Integer.toHexString(iParam));
+                        
+                        OnDeviceMsg(iLine, iEvent, iParam);
+                        
                     });
                 });
 
@@ -102,18 +108,25 @@ public class AD101Tester extends Application {
         btnLeds.setText("Led Checks");
         btnLeds.setOnAction((ActionEvent t) -> {
             btnLeds.setDisable(true);
-            int i = 0;
-            for (String msg : msgs) {
-                cid.AD101_SetLED(2, i++);
-                out.println(msg);
-                btnLeds.setText(msg);
-                try {
-                    Thread.sleep(3000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(AD101Tester.class.getName()).log(Level.SEVERE, null, ex);
+
+            Thread t1 = new Thread(() -> {
+                int i = 0;
+                for (String msg : msgs) {
+                    cid.AD101_SetLED(0, i++);
+                    out.println(msg);
+                    btnLeds.setText(msg);
+                    try {
+                        Thread.sleep(3000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(AD101Tester.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
-            }
-            btnLeds.setDisable(false);
+
+                Platform.runLater(() -> {
+                    btnLeds.setDisable(false);
+                });
+
+            });
 
         });
 
@@ -130,6 +143,196 @@ public class AD101Tester extends Application {
         primaryStage.setTitle("CID Tester");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    private void OnDeviceMsg(int nLine, int iEvent, int iParam) {
+        switch (iEvent) {
+            case MCU_BACKDISABLE:
+                //
+                break;
+
+            case MCU_BACKENABLE:
+                //
+                break;
+
+            case MCU_BACKID: {
+                String cpuVersion = "";
+                cid.AD101_GetCPUVersion(nLine, cpuVersion);
+            }
+            break;
+
+            case MCU_BACKCID: {
+                String szCallerID = new String();
+                String szName = new String();
+                String szTime = new String();
+
+                int nLen = cid.AD101_GetCallerID(nLine, szCallerID, szName, szTime);
+                out.println("Caller ID:" + szCallerID + ", Name: " + szName);
+            }
+            break;
+
+            case MCU_BACKSTATE: {
+                switch (iParam) {
+                    case HKONSTATEPRA:
+                        //
+                        out.println("HOOK ON PR+");
+                        break;
+
+                    case HKONSTATEPRB:
+                        out.println("HOOK ON PR-");
+                        break;
+
+                    case HKONSTATENOPR:
+                        out.println("HOOK ON NOPR");
+                        break;
+
+                    case HKOFFSTATEPRA: {
+                        out.println("HOOK OFF PR+");
+                        String szCallerID = new String();
+                        String szName = new String();
+                        String szTime = new String();
+
+                        if (cid.AD101_GetCallerID(nLine, szCallerID, szName, szTime) < 1 || cid.AD101_GetRingIndex(nLine) < 1) {
+                            //set null for phone id
+                        }
+
+                        //null phone id
+                    }
+                    break;
+
+                    case HKOFFSTATEPRB: {
+                        out.println("HOOK OFF PR-");
+
+                        String szCallerID = new String();
+                        String szName = new String();
+                        String szTime = new String();
+
+                        if (cid.AD101_GetCallerID(nLine, szCallerID, szName, szTime) < 1 || cid.AD101_GetRingIndex(nLine) < 1) {
+                            //set null phone id
+                        }
+
+                    }
+                    break;
+
+                    case NO_LINE: {
+                        out.println("No Line");
+
+                        String szCallerID = new String();
+                        String szName = new String();
+                        String szTime = new String();
+
+                        if (cid.AD101_GetCallerID(nLine, szCallerID, szName, szTime) < 1 || cid.AD101_GetRingIndex(nLine) < 1) {
+                            //no line
+                        }
+
+                        //no line
+                    }
+                    break;
+
+                    case RINGONSTATE: {
+                        String szRing = "Ring:" + String.format("{0:D2}", cid.AD101_GetRingIndex(nLine));
+                        out.println(szRing);
+                    }
+                    break;
+
+                    case RINGOFFSTATE:
+                        out.println("Ring Off");
+                        break;
+
+                    case NOHKPRA:
+                        out.println("NO HOOK PR+");
+                        break;
+
+                    case NOHKPRB:
+                        out.println("NO HOOK PR-");
+                        break;
+
+                    case NOHKNOPR: {
+                        out.println("NO HOOK NOPR");
+
+                        String szCallerID = new String();
+                        String szName = new String();
+                        String szTime = new String();
+
+                        if (cid.AD101_GetCallerID(nLine, szCallerID, szName, szTime) < 1 || cid.AD101_GetRingIndex(nLine) < 1) {
+                            //not found
+                        }
+
+                        //set null
+                    }
+                    break;
+
+                    default:
+                        break;
+                }
+            }
+            break;
+
+            case MCU_BACKDIGIT: {
+                String szDialDigit = new String();
+                cid.AD101_GetDialDigit(nLine, szDialDigit);
+                out.println("Dial digit: " + szDialDigit);
+            }
+            break;
+
+            case MCU_BACKCOLLATERAL: {
+                String szDialDigit = new String();
+
+                cid.AD101_GetCollateralDialDigit(nLine, szDialDigit);
+                out.println("DialDigit : " + szDialDigit);
+            }
+            break;
+
+            case MCU_BACKDEVICE: {
+                String szCPUVersion = new String();
+
+                //enable
+                cid.AD101_GetCPUVersion(nLine, szCPUVersion);
+                out.println("Version: " + szCPUVersion);
+
+            }
+            break;
+
+            case MCU_BACKPARAM: {
+                AD101DEVICEPARAMETER tagParameter = new AD101DEVICEPARAMETER();
+
+                cid.AD101_GetParameter(nLine, tagParameter);
+
+                out.println(tagParameter.nRingOn);
+                out.println(tagParameter.nRingOff);
+                out.println(tagParameter.nHookOn);
+                out.println(tagParameter.nHookOff);
+                out.println(tagParameter.nStopCID);
+                out.println(tagParameter.nNoLine);
+            }
+            break;
+
+            case MCU_BACKCPUID: {
+                String szCPUID = new String();
+
+                cid.AD101_GetCPUID(nLine, szCPUID);
+
+                //cpu id
+                out.println("CPUID : " + szCPUID);
+
+            }
+            break;
+
+            case MCU_BACKMISSED: {
+                out.println("Missed Call");
+            }
+            break;
+
+            case MCU_BACKTALK: {
+                String strTalk;
+                strTalk = String.format("{0:D2}", iParam) + "S";
+                out.println(strTalk);
+            }
+            break;
+
+            default:
+                break;
+        }
     }
 
     @Override
